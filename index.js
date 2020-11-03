bc2049f8628750fcef69d6537fcb3e9191b48701
toserjude <toser.jude@gmail.com>
2020-11-03 15:31:43 +0100

    add node server to project for heroku

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(cors());
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.use(express.static('./dist/<name-on-package.json>'));
