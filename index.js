7d9d38beb064661a526540ed079707b94d318331
toserjude <toser.jude@gmail.com>
2020-11-03 16:01:25 +0100

    fixes

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
+
 const app = express();
-const cors = require('cors');
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.use(cors());
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
 app.use(express.static('./dist/fe-app'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/fe-app/'}),
+);
+
+app.listen(process.env.PORT || 8080);
